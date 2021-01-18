e0cb876416003b030d47ce7a7e58b4747b0e5dfd
MamathaBeepangi <77383005+MamathaBeepangi@users.noreply.github.com>
2021-01-18 18:01:19 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ class helloworld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello Mamatha welcome to jenkins");
-		System.out.println("This is example for pol scm");
+		System.out.println("This is example for poll scm");
+		System.out.println("Thank You");
 	}
 }
